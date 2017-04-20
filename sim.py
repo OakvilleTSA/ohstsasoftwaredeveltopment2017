5657b1d28390915e8129192eb031ba65edbdcd5d
animus-v0x <weatherfreak10@gmail.com>
2017-04-20 09:30:00 -0500

    Added sim.py

---
+++ sim.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+'''
+ ____ _____ _   _ ____  _____ _   _ _____
+/ ___|_   _| | | |  _ \| ____| \ | |_   _|
+\___ \ | | | | | | | | |  _| |  \| | | |
+ ___) || | | |_| | |_| | |___| |\  | | |
+|____/ |_|  \___/|____/|_____|_| \_| |_|
+
+ ___ _   _ _____ ___  ____  __  __    _  _____ ___ ___  _   _
+|_ _| \ | |  ___/ _ \|  _ \|  \/  |  / \|_   _|_ _/ _ \| \ | |
+ | ||  \| | |_ | | | | |_) | |\/| | / _ \ | |  | | | | |  \| |
+ | || |\  |  _|| |_| |  _ <| |  | |/ ___ \| |  | | |_| | |\  |
+|___|_| \_|_|   \___/|_| \_\_|  |_/_/   \_\_| |___\___/|_| \_|
+
+ __  __    _    _   _    _    ____ _____ ____
+|  \/  |  / \  | \ | |  / \  / ___| ____|  _ \
+| |\/| | / _ \ |  \| | / _ \| |  _|  _| | |_) |
+| |  | |/ ___ \| |\  |/ ___ \ |_| | |___|  _ <
+|_|  |_/_/   \_\_| \_/_/   \_\____|_____|_| \_\
+
+OAKVILLE TSA PROJECT 2017
+
+Student Information Manager (SIM)
+Manages student information from a teacher/administrative perspective while also
+allowing the student to take tests, look at grades, look at homework, email,and
+more. Parents also can see their kids progress in school.
+
+Credits:
+Garrett Summerfield - i <3 linux
+Thomas Gleiforst - Best Programmer here
+Hannah Jokuti - Thomas's partner in crime
+Alisa Lazareva - LASANGAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+
+'''
