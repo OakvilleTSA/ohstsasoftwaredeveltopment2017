12d73e4ff2b86ca3266f40300bb98253f44ff35d
ps2dude7 <ps2dude67@sbcglobal.net>
2017-04-22 00:13:02 -0500

    Added Core SIM Structure
    Added the basic layout of the student screen, and coded the viewGrades
    button (1/4 completed)

--- sim.py
+++ sim.py
@@ -45,7 +45,7 @@ from Tkinter import *
 ############################################
 key = []
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-users = [{'username': 'admin', 'grades': {'English': 100, 'Math': 100, 'Social Studies': 100, 'Science': 100}, 'password': '', 'code': []}]
+users = [{'username': '', 'grades': {'English': 100, 'Math': 100, 'Social Studies': 100, 'Science': 100}, 'password': '', 'code': [], 'first': 'ad', 'last': 'min', 'role': 'student'}]
 
 
 
@@ -90,28 +90,27 @@ def loginAttempt():
                 #open next screen
                 print 'In the system'
                 match = True
-                sim()
+                sim(account)
     else:
         if match == False:
             newUser(userid)
         
 def newUser(userid):
-    root = Tkinter.Tk()
-    root.title("SIM - New User")
-    root.overrideredirect(False)
-    width = root.winfo_screenwidth()
-    height = root.winfo_screenheight()
-    root.geometry('%dx%d+%d+%d' % (width*0.4, height*0.4, width*0.1, height*0.1))
-    Label(root, text="New User Detected", font=("Arial", 20)).grid(row=0, column=0)
-    Label(root, text="Please indicate if you are a student or teacher", font=("Arial", 15)).grid(row=1, column=0)
-    role = ''
-    Radiobutton(root, text="Teacher", value="teacher", variable=role).grid(row=2, column=0)
-    Radiobutton(root, text="Student", value="student", variable=role).grid(row=3, column=0)
-    Tkinter.Label(root, text="First Name:").grid(row=4, column=0)
-    ent1 = Tkinter.Entry(root)
+    rootLogin.destroy()
+    rootNew = Tkinter.Tk()
+    rootNew.title("SIM - New User")
+    rootNew.overrideredirect(False)
+    rootNew.minsize(400, 250)
+    Label(rootNew, text="New User Detected", font=("Arial", 20)).grid(row=0, column=0)
+    Label(rootNew, text="Please indicate if you are a student or teacher", font=("Arial", 15)).grid(row=1, column=0)
+    role = '' #####THESE ARE THE LINES NOT WORKING - ROLE WON'T CHANGE#####
+    Radiobutton(rootNew, text="Teacher", value="teacher", variable=role).grid(row=2, column=0)#####
+    Radiobutton(rootNew, text="Student", value="student", variable=role).grid(row=3, column=0)#####
+    Tkinter.Label(rootNew, text="First Name:").grid(row=4, column=0)
+    ent1 = Tkinter.Entry(rootNew)
     ent1.grid(row=5, column=0)
-    Tkinter.Label(root, text="Last Name:").grid(row=6, column=0)
-    ent2 = Tkinter.Entry(root)
+    Tkinter.Label(rootNew, text="Last Name:").grid(row=6, column=0)
+    ent2 = Tkinter.Entry(rootNew)
     ent2.grid(row=7, column=0)
     
     ###Odd placement but this is where it has to be
@@ -121,37 +120,36 @@ def newUser(userid):
                 'password': password,
                 'first': ent1.get(),
                 'last': ent2.get(),
-                'role': role.get(),
+                'role': role.get(),#####
                 'code': key,
                 'grades': { 'English': 0,
                             'Math': 0,
                             'Social Studies': 0,
                             'Science': 0 } }
         users.append(user)
-        print user
+        print user ##For testing purposes##
+        rootNew.destroy()
     ###
     
-    create = Tkinter.Button(root, text="Create", command=create)
+    create = Tkinter.Button(rootNew, text="Create", command=create)
     create.grid(row=8, column=0)
 
 ####################################
 #CANVAS, WIDGETS, AND DRAWINGS HERE#
 ####################################
 #Basic initlization
-rootMain = Tkinter.Tk()
-rootMain.title("SIM - Student Information Manager")
-rootMain.minsize(600,600)
-canvas = Tkinter.Canvas(rootMain, width=900, height=600, background='#FFFFFF')
-canvas.grid(row=0, rowspan=50, column=4)
-title = Tkinter.Label(rootMain, text="Student \nInformation \nManager", font=("Arial", 30))
+rootLogin = Tkinter.Tk()
+rootLogin.title("SIM - Student Information Manager")
+rootLogin.minsize(200,300)
+title = Tkinter.Label(rootLogin, text="Student \nInformation \nManager", font=("Arial", 30))
 title.grid(row=0,column=0)
 
 #Username and Password Entry Boxes (WIP)
-usernameL = Tkinter.Label(rootMain, text="Username:")
-usernameB = Tkinter.Entry(rootMain)
-passwordL = Tkinter.Label(rootMain, text="Password:")
-passwordB = Tkinter.Entry(rootMain)
-submit = Tkinter.Button(rootMain, text="Submit", command=loginAttempt)
+usernameL = Tkinter.Label(rootLogin, text="Username:")
+usernameB = Tkinter.Entry(rootLogin)
+passwordL = Tkinter.Label(rootLogin, text="Password:")
+passwordB = Tkinter.Entry(rootLogin)
+submit = Tkinter.Button(rootLogin, text="Submit", command=loginAttempt)
 
 #Place widgets on grid
 usernameL.grid(row=1,column=0)
@@ -167,14 +165,44 @@ submit.grid(row=5,column=0)
 #MORE FUNCTIONS HERE#
 #####################
 '''For functions that need to be AFTER the initlization of the canvas'''
-def sim():
-    draw
 
+def sim(account):
+    def viewGrades():
+        rootGrades = Tkinter.Tk()
+        rootGrades.title("SIM - Student Information Manager")
+        rootGrades.minsize(400,400)
+        grades = account['grades']
+        Tkinter.Label(rootGrades, text="Student Information Manager", font=("Arial", 30)).grid(row=0, column=0, columnspan=3)
+        Tkinter.Label(rootGrades, text=account['first'] + " " + account['last'], font=("Arial", 20)).grid(row=1, column=0, columnspan=3)
+        Tkinter.Label(rootGrades, text='GRADES:', font=("Arial", 15)).grid(row=2, column=1)
+        Tkinter.Label(rootGrades, text='English: ' + str(grades['English']), font=("Arial", 15)).grid(row=3, column=1)
+        Tkinter.Label(rootGrades, text='Math: ' + str(grades['Math']), font=("Arial", 15)).grid(row=4, column=1)
+        Tkinter.Label(rootGrades, text='Social Studies: ' + str(grades['Social Studies']), font=("Arial", 15)).grid(row=5, column=1)
+        Tkinter.Label(rootGrades, text='Science: ' + str(grades['Science']), font=("Arial", 15)).grid(row=6, column=1)
 
+    def takeTest():
+        print 'Test taken'
+    def vAssignments():
+        print 'Assignments viewed'
+    def cAssignments():
+        print 'Assignments completed'    
+    rootLogin.destroy()
+    rootSim = Tkinter.Tk()
+    rootSim.title("SIM - Student Information Manager")
+    rootSim.minsize(400,400)
+    if account['role'] == 'student':
+        Tkinter.Label(rootSim, text="Student Information Manager", font=("Arial", 30)).grid(row=0, column=0, columnspan=2)
+        Tkinter.Label(rootSim, text=account['first'] + " " + account['last'], font=("Arial", 20)).grid(row=1, column=0, columnspan=2)
+        grades = Tkinter.Button(rootSim, text="View \nGrades", command=viewGrades, width=15, height=5, bg="light gray", font=("Arial", 20)).grid(row=2, column=0)
+        test = Tkinter.Button(rootSim, text="Take A \nTest", command=takeTest, width=15, height=5, bg="light gray", font=("Arial", 20)).grid(row=2, column=1)
+        Tkinter.Label(rootSim).grid(row=3)
+        viewAssignments = Tkinter.Button(rootSim, text="View \nAssignments", command=vAssignments, width=15, height=5, bg="light gray", font=("Arial", 20)).grid(row=4, column=0)
+        createAssignments = Tkinter.Button(rootSim, text="Complete an \nAssignment", command=cAssignments, width=15, height=5, bg="light gray", font=("Arial", 20)).grid(row=4, column=1)
 
+    
 
 ###############################
 #MUST BE THE LAST LINE OF CODE#
 ############################### 
 def start():
-    rootMain.mainloop()
+    rootLogin.mainloop()
